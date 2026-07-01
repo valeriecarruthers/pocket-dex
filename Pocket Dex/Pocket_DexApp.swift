2e000329352e4eae8f06190f9cd62ca589f8d738
Valerie Carruthers <6935342+CorvusCali@users.noreply.github.com>
2026-07-01 11:14:31 -0400

    Initial Commit

---
+++ Pocket Dex/Pocket_DexApp.swift
@@ -0,0 +1,32 @@
+//
+//  Pocket_DexApp.swift
+//  Pocket Dex
+//
+//  Created by Valerie Carruthers on 2026-07-01.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Pocket_DexApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Pocket DexTests/Pocket_DexTests.swift
@@ -0,0 +1,18 @@
+//
+//  Pocket_DexTests.swift
+//  Pocket DexTests
+//
+//  Created by Valerie Carruthers on 2026-07-01.
+//
+
+import Testing
+
+struct Pocket_DexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
